2afd6096e658d8339221663a5d317cb3e5c4b878
joaquingomez3 <gomezgonzalod46@gmail.com>
2024-05-30 18:49:37 -0300

    metodos

--- src/Arbol.java
+++ src/Arbol.java
@@ -1,4 +1,69 @@
 
 public class Arbol {
+    Nodo raiz;
+
+    public Arbol() {
+        this.raiz = null;
+    }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo raiz) {
+        this.raiz = raiz;
+    }
     
+    //metodo insertar nodo
+    public void insertarN(int valor1){
+        Nodo nuevo=new Nodo(valor1);
+        if (raiz==null){
+            raiz=nuevo;
+           
+        } else{ 
+            Nodo aux =raiz;
+            Nodo padre;
+            while(true){
+            padre=aux;    
+            if (valor1<aux.valor){
+                aux=aux.izq;
+             if(aux==null){
+                padre.izq=nuevo;
+                return;
+             }
+            } else{
+                aux=aux.der;
+                 if(aux==null){
+                padre.der=nuevo;
+                return;
+             }
+            }
+            }
+        }
+         
+    }
+    //metodo busqueda de nodo
+    public Nodo busquedaN( int valor){
+       Nodo punt=raiz; 
+       
+        while(punt.valor != valor){
+            if(valor<punt.valor){
+                punt=punt.izq;
+            }else{
+                punt=punt.der;
+            }
+            if(punt==null){
+                return null;
+            }
+        }
+    return punt;
+}
+    //recorrido de nodo
+    public void Recorrido(Nodo raiz){
+        if(raiz!=null){
+            System.out.println(raiz.valor);
+            Recorrido(raiz.izq);
+            Recorrido(raiz.der);
+        }
+    }
 }
--- src/Nodo.java
+++ src/Nodo.java
@@ -1,4 +1,39 @@
 
 public class Nodo {
+    int valor;
+    Nodo izq;
+    Nodo der;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        this.izq = null;
+        this.der = null;
+    }
+
+    public Nodo() {
+    }
     
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getIzq() {
+        return izq;
+    }
+
+    public void setIzq(Nodo izq) {
+        this.izq = izq;
+    }
+
+    public Nodo getDer() {
+        return der;
+    }
+
+    public void setDer(Nodo der) {
+        this.der = der;
+    }
 }
